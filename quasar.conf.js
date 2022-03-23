a9243f411b05acc6b02be94db9e26411d4270cda
Jeff Galbraith <galbraith64@gmail.com>
2022-03-23 16:47:53 -0600

    chore: update deps

--- quasar.conf.js
+++ quasar.conf.js
@@ -35,6 +35,7 @@ module.exports = configure(function (ctx) {
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
+      'material-icons',
       // 'ionicons-v4',
       'mdi-v6',
       // 'fontawesome-v5',
