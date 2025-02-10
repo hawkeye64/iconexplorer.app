50d84b2382a076f1b4251c1d878552bf24c538cc
Jeff <galbraith64@gmail.com>
2025-02-10 13:10:52 -0700

    task: updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import pluginVue from 'eslint-plugin-vue'
 import pluginQuasar from '@quasar/app-vite/eslint'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import prettierSkipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
@@ -21,20 +21,6 @@ export default [
   ...pluginQuasar.configs.recommended(),
   js.configs.recommended,
 
-  /**
-   * https://eslint.vuejs.org
-   *
-   * pluginVue.configs.base
-   *   -> Settings and rules to enable correct ESLint parsing.
-   * pluginVue.configs[ 'flat/essential']
-   *   -> base, plus rules to prevent errors or unintended behavior.
-   * pluginVue.configs["flat/strongly-recommended"]
-   *   -> Above, plus rules to considerably improve code readability and/or dev experience.
-   * pluginVue.configs["flat/recommended"]
-   *   -> Above, plus rules to enforce subjective community defaults to ensure consistency.
-   */
-  ...pluginVue.configs['flat/essential'],
-
   {
     files: ['**/*.ts', '**/*.vue'],
     rules: {
@@ -42,20 +28,26 @@ export default [
     },
   },
   // https://github.com/vuejs/eslint-config-typescript
-  ...vueTsEslintConfig({
+  ...defineConfigWithVueTs(
+    /**
+     * https://eslint.vuejs.org
+     *
+     * pluginVue.configs.base
+     *   -> Settings and rules to enable correct ESLint parsing.
+     * pluginVue.configs[ 'flat/essential']
+     *   -> base, plus rules to prevent errors or unintended behavior.
+     * pluginVue.configs["flat/strongly-recommended"]
+     *   -> Above, plus rules to considerably improve code readability and/or dev experience.
+     * pluginVue.configs["flat/recommended"]
+     *   -> Above, plus rules to enforce subjective community defaults to ensure consistency.
+     */
+    pluginVue.configs['flat/essential'],
     // Optional: extend additional configurations from typescript-eslint'.
     // Supports all the configurations in
     // https://typescript-eslint.io/users/configs#recommended-configurations
-    extends: [
-      // By default, only the 'recommendedTypeChecked' rules are enabled.
-      'recommendedTypeChecked',
-      // You can also manually enable the stylistic rules.
-      // "stylistic",
-
-      // Other utility configurations, such as 'eslintRecommended', (note that it's in camelCase)
-      // are also extendable here. But we don't recommend using them directly.
-    ],
-  }),
+    // We STRONGLY RECOMMEND you to start with `recommended` or `recommendedTypeChecked`.
+    vueTsConfigs.recommendedTypeChecked,
+  ),
 
   {
     languageOptions: {
